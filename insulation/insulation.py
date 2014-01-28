2b8f91401a8f4581077aa1861f7436c3611bac8e
Saulo Ricci <infsaulok@gmail.com>
2014-01-28 10:59:15 -0200

    insulation module
    uploading insulation module

---
+++ insulation/insulation.py
@@ -0,0 +1,14 @@
+# Description: insulation.py defines the class that try insulates each module at our architeture.
+# Date: 2010/08/23
+# Author: Saulo Marques Ribeiro Ricci
+
+class insulation:
+	# input_set is a dictionary that contains information about files.
+	def __init__(self, input_set, output_set):
+		self.input_set = input_set
+		self.output_set = output_set
+
+	# executes the mainly function based in input_set, and produces the output_set
+	def execute(self):
+		pass
+
